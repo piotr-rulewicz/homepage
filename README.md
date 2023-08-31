a65cd7406e3f8769bd5249180cfcb2b6679bda7c
Piotr Rulewicz <piotr.rulewicz@hotmail.com>
2023-08-31 23:44:32 +0200

    Updated README, added pics, new init function

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
 # Piotr Rulewicz - my first page
 
 ## Description
-This is my first website made to practice skills in HTML, CSS and Javascript.
-The **BEM** convention and **normalize.css** were used. I invite you to familiarize yourself with my work.
+
+This is my first website made to practice HTML, CSS and Javascript skills.<br>
+The **BEM** convention and **normalize.css** were used. In Javascript code I used **arrow functions**.<br>
+I invite you to familiarize yourself with my work.
+
+## What can you find here and how it's working
+
+I've posted here the solutions to the webdev course from **"Youcode"**. Lessons **2 to 5**.<br>
+There are two buttons on the page. One changes the color of the header. The second changes the background color.<br>
+Have a fun!
+
+## Screenshots
+
+**Screenshot 1:** default header color
+
+![Screenshot 1: default header color](/img/6_screen-homepage_readme.jpg)
+
+**Screenshot 2:** alternative header color
+
+![Screenshot 2: alternative header color](/img/7_screen-homepage_readme.jpg)
+
+**Screenshot 3:** default background color
+
+![Screenshot 3: default background color](/img/8_screen-homepage_readme.jpg)
+
+**Screenshot 4:** alternative background color
+
+![Screenshot 4: alternative background color](/img/9_screen-homepage_readme.jpg)
 
 ## Demo
+
 https://piotr-rulewicz.github.io/homepage/
--- js/script.js
+++ js/script.js
@@ -1,45 +1,46 @@
 {
 
-const welcome = () => {
-  console.log("Hello, World!");
-}
+  const welcome = () => {
+    console.log("Hello, World!");
+  }
 
-welcome();
+  welcome();
 
-const init = () => {
+  const init = () => {
 
-const toogleHeaderColor = () => {
-  const switchHeaderButton = document.querySelector(".article__switchHeaderButton");
-  const mainHeader = document.querySelector(".header");
-  const mainHeaderDarker = document.querySelector(".js-article__switchHeaderButton");
-  
-  switchHeaderButton.addEventListener("click", () => {
-    mainHeader.classList.toggle("header--darker");
-    
-    mainHeaderDarker.innerText =
-    mainHeader.classList.contains("header--darker") ? "domyślny" : "inny";
-});
-}
+    const toggleHeaderColor = () => {
+      const switchHeaderButton = document.querySelector(".article__switchHeaderButton");
+      const mainHeader = document.querySelector(".header");
+      const mainHeaderDarker = document.querySelector(".js-article__switchHeaderButton");
 
-toogleHeaderColor();
+      const handleHeaderClick = () => {
+        mainHeader.classList.toggle("header--darker");
+        mainHeaderDarker.innerText =
+          mainHeader.classList.contains("header--darker") ? "domyślny" : "inny";
+      };
 
-const toogleBackgroundColor = () => {
-  const switchBackgroundButton = document.querySelector(".article__switchBackgroundButton");
-  const body = document.querySelector(".body");
-  const bodyDarker = document.querySelector(".js-article__switchBackgroundButton");
-  
-  switchBackgroundButton.addEventListener("click", () => {
-    body.classList.toggle("body--darker");
+      switchHeaderButton.addEventListener("click", handleHeaderClick);
+    }
 
-    bodyDarker.innerText =
-    body.classList.contains("body--darker") ? "domyślny" : "inny";
-});
-}
+    const toggleBackgroundColor = () => {
+      const switchBackgroundButton = document.querySelector(".article__switchBackgroundButton");
+      const body = document.querySelector(".body");
+      const bodyDarker = document.querySelector(".js-article__switchBackgroundButton");
 
-toogleBackgroundColor();
+      const handleBackgroundClick = () => {
+        body.classList.toggle("body--darker");
+        bodyDarker.innerText =
+          body.classList.contains("body--darker") ? "domyślny" : "inny";
+      };
 
-}
+      switchBackgroundButton.addEventListener("click", handleBackgroundClick);
+    }
 
-init();
+    toggleHeaderColor();
+    toggleBackgroundColor();
+
+  }
+
+  init();
 
 }
