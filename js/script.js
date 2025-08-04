70e9239d6d006111a557b2a1f5c7b79a7f122eed
Piotr Rulewicz <piotr.rulewicz@hotmail.com>
2025-08-04 22:11:22 +0200

    chore: configure Prettier and ESLint; add formatting scripts; format project next files

--- js/script.js
+++ js/script.js
@@ -1,46 +1,42 @@
 {
-
   const welcome = () => {
-    console.log("Hello, World!");
-  }
+    console.log('Hello, World!');
+  };
 
   welcome();
 
   const init = () => {
-
     const toggleHeaderColor = () => {
-      const switchHeaderButton = document.querySelector(".article__switchHeaderButton");
-      const mainHeader = document.querySelector(".header");
-      const mainHeaderDarker = document.querySelector(".js-article__switchHeaderButton");
+      const switchHeaderButton = document.querySelector('.article__switchHeaderButton');
+      const mainHeader = document.querySelector('.header');
+      const mainHeaderDarker = document.querySelector('.js-article__switchHeaderButton');
 
       const handleHeaderClick = () => {
-        mainHeader.classList.toggle("header--darker");
-        mainHeaderDarker.innerText =
-          mainHeader.classList.contains("header--darker") ? "domyślny" : "inny";
+        mainHeader.classList.toggle('header--darker');
+        mainHeaderDarker.innerText = mainHeader.classList.contains('header--darker')
+          ? 'domyślny'
+          : 'inny';
       };
 
-      switchHeaderButton.addEventListener("click", handleHeaderClick);
-    }
+      switchHeaderButton.addEventListener('click', handleHeaderClick);
+    };
 
     const toggleBackgroundColor = () => {
-      const switchBackgroundButton = document.querySelector(".article__switchBackgroundButton");
-      const body = document.querySelector(".body");
-      const bodyDarker = document.querySelector(".js-article__switchBackgroundButton");
+      const switchBackgroundButton = document.querySelector('.article__switchBackgroundButton');
+      const body = document.querySelector('.body');
+      const bodyDarker = document.querySelector('.js-article__switchBackgroundButton');
 
       const handleBackgroundClick = () => {
-        body.classList.toggle("body--darker");
-        bodyDarker.innerText =
-          body.classList.contains("body--darker") ? "domyślny" : "inny";
+        body.classList.toggle('body--darker');
+        bodyDarker.innerText = body.classList.contains('body--darker') ? 'domyślny' : 'inny';
       };
 
-      switchBackgroundButton.addEventListener("click", handleBackgroundClick);
-    }
+      switchBackgroundButton.addEventListener('click', handleBackgroundClick);
+    };
 
     toggleHeaderColor();
     toggleBackgroundColor();
-
-  }
+  };
 
   init();
-
 }
